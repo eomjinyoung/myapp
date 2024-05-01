9669332404e0caddd2b83cc0851d2f24249c0117
eomjinyoung <jinyoung.eom@gmail.com>
2024-05-01 17:42:44 +0900

    .

--- app/src/main/java/bitcamp/myapp/security/SecurityConfig.java
+++ app/src/main/java/bitcamp/myapp/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package bitcamp.myapp.security;
 
-import bitcamp.myapp.controller.AssignmentController;
 import bitcamp.myapp.service.MemberService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -9,16 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
 
   private static final Log log = LogFactory.getLog(SecurityConfig.class);
@@ -36,13 +31,13 @@ public class SecurityConfig {
             .anyRequest().authenticated()
         )
         .formLogin(formLoginConfigurer -> {
-            formLoginConfigurer
-                .loginPage("/auth/form") // 로그인 폼을 제공하는 URL
-                .loginProcessingUrl("/auth/login") // 클라이언트가 로그인을 하기 위해 요청하는 URL (페이지 컨트롤러와 상관없다)
-                .usernameParameter("email") // 로그인 수행할 때 사용할 사용자 아이디 또는 이메일(principal) 파라미터 명
-                .passwordParameter("password") // 로그인 수행할 때 사용할 사용자 암호(credential) 파라미터 명
-                .defaultSuccessUrl("/home", true) // 로그인 성공 후 리다이렉트 할 URL
-                .permitAll(); // 모든 권한 부여
+          formLoginConfigurer
+              .loginPage("/auth/form") // 로그인 폼을 제공하는 URL
+              .loginProcessingUrl("/auth/login") // 클라이언트가 로그인을 하기 위해 요청하는 URL (페이지 컨트롤러와 상관없다)
+              .usernameParameter("email") // 로그인 수행할 때 사용할 사용자 아이디 또는 이메일(principal) 파라미터 명
+              .passwordParameter("password") // 로그인 수행할 때 사용할 사용자 암호(credential) 파라미터 명
+              .defaultSuccessUrl("/home", true) // 로그인 성공 후 리다이렉트 할 URL
+              .permitAll(); // 모든 권한 부여
         });
     return http.build();
   }
